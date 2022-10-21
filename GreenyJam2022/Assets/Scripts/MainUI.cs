8d80292de69db76496787580aec43ac43adc1933
nilayozkir <112647930+nilayozkir@users.noreply.github.com>
2022-10-22 01:08:44 +0300

    uı

--- GreenyJam2022/Assets/Scripts/MainUI.cs
+++ GreenyJam2022/Assets/Scripts/MainUI.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+    public static bool _gamePaused=false;
+    public GameObject pause;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        pause.SetActive(false);
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause.SetActive(true); 
+            if (_gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+           
+        }
+    }
+
+    void PauseGame()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        _gamePaused=true;
+    }
+
+  public  void ResumeGame()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        _gamePaused = false;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+       
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit game");
+        Application.Quit();
     }
 }
